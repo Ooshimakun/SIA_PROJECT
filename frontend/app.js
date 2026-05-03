3241c9b4a1a0bf606156b6767a9f686edb273dba
Ooshima_kun <reykennethlapure27@gmail.com>
2026-05-03 10:20:55 +0800

    updated the frontend to use Cloud API

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,7 @@ document.getElementById("borrowForm").addEventListener("submit", async (e) => {
 
   try {
     // Send JSON data to the Borrowing System API
-    const response = await fetch("http://127.0.0.1:5001/borrow", {
+    const response = await fetch("https://borrowing-api.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
